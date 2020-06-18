e81d999a64e053d97fa4c650286cf8d16085cca1
haxul <s.s.starodubov@yandex.ru>
2020-06-18 21:25:48 +0400

    init actor akka

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -1,8 +1,40 @@
 package playground
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object Playground extends App {
-  val actorSystem = ActorSystem("HelloAkka")
-  println(actorSystem.name)
+  val actorSystem = ActorSystem("akkaActor")
+
+  class WordCounter extends Actor {
+    var totalWords = 0
+    override def receive: Receive = {
+      case message: String =>
+        println(s"I got a message: $message")
+        totalWords += message.split(" ").length
+      case msg => println(s"Unknown message $msg")
+    }
+  }
+
+  val wordCounter: ActorRef = actorSystem.actorOf(Props[WordCounter], "WordCounter")
+
+  object Person {
+    def props(name:String):Props = Props(new Person(name))
+  }
+  
+  class Person(name:String) extends Actor {
+    override def receive: Receive = {
+      case "name" => println(s"my name is $name")
+      case message:String => println(s"i got $message")
+      case list: List[_] => println(list)
+      case _  => println("list of string")
+    }
+  }
+
+  val person = actorSystem.actorOf(Person.props("sergei"))
+
+  person ! "123213"
+  person ! "name"
+  person ! List(1,23)
+  person ! List("hell ow")
 }
+
